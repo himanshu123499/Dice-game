03d62ac4b2c67deaa11c07e1e6b48d6b24bcafd4
himanshu123499 <77163752+himanshu123499@users.noreply.github.com>
2023-11-05 16:53:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var random1= Math.round((Math.random()*5)+1);
+var random2= Math.round((Math.random()*5)+1);
+
+var randomdice1="images/dice"+random1+".png";
+var randomdice2="images/dice"+random2+".png";
+
+const imageselct=document.querySelectorAll("img");
+imageselct[0].setAttribute("src",randomdice1);
+imageselct[1].setAttribute("src",randomdice2);
+
+
+const head=document.querySelector("h1");
+
+if(random1>random2){
+    head.innerHTML="player 1 wins";
+}
+else if(random2>random1){
+    head.innerHTML="player 2 wins";
+}
+else{
+    head.innerHTML="tie";
+}
